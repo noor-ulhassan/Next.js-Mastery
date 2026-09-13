322a4ffe5cd7927ea1b933197410ad834ea018fe
Noor <thenoorulhassan.dev@gmail.com>
2026-09-13 23:15:41 +0500

    destination page

--- practice-project1/src/app/page.jsx
+++ practice-project1/src/app/page.jsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex  items-center justify-center text-5xl  text-red-500 h-full ">
+      Welcome to our Travel Guide
+    </div>
   );
 }
--- practice-project1/src/components/Nav.jsx
+++ practice-project1/src/components/Nav.jsx
@@ -1,24 +1,37 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 function Nav() {
+  const pathname = usePathname();
+
   return (
-    <div className="w-full h-16 bg-white flex justify-between">
+    <div className="w-full h-16 bg-white flex justify-between fixed top-0">
       <div className="w-full max-w-350 mx-auto py-5 px-2 text-black font-bold text-lg">
         🌏 Travel Guide
       </div>
       <div className=" text-black font-medium text-sm ">
         <ul className="justify-between flex items-center gap-8 py-6 pr-6 ">
-          <Link href="/">
+          <Link href="/" className={pathname === "/" ? "text-orange-500" : ""}>
             <li>Home</li>
           </Link>
-          <Link href="/destination">
+          <Link
+            href="/destination"
+            className={pathname === "/destination" ? "text-orange-500" : ""}
+          >
             <li>Destination</li>
           </Link>
-          <Link href="/about">
+          <Link
+            href="/about"
+            className={pathname === "/about" ? "text-orange-500" : ""}
+          >
             <li>About</li>
           </Link>
-          <Link href="/contact">
+          <Link
+            href="/contact"
+            className={pathname === "/contact" ? "text-orange-500" : ""}
+          >
             <li>Contact</li>
           </Link>
         </ul>
